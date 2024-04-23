a6f137f93cf585f4d7985ad0f6f9a1ab7d5473ca
Ed Parsons <ed@acutulus.co>
2024-04-23 14:51:29 -0700

    adding base t&c and privacy pages

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/src/lib/utils"
+
+export default async function Page() {
+  return (
+    <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <div className="mx-auto w-full min-w-0">
+        <div className="space-y-2">
+          <h1 className={cn("scroll-m-20 text-4xl font-bold tracking-tight")}>
+            Privacy Policy
+          </h1>
+          <p>TBD</p>
+        </div>
+      </div>
+    </main>
+  )
+}
